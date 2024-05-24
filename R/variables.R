f10ead550b8548615df024e92cd697782deaa5b5
Grinnell <Matthew.Grinnell@dfo-mpo.gc.ca>
2024-05-24 13:04:26 -0700

    Add variables

--- R/variables.R
+++ R/variables.R
@@ -8,3 +8,13 @@ sim_dat <- sim_fsar_data("wide") %>%
     F_max = `Fhigh-1/yr`, F_lim = `Flim-1/yr`, M_med = `M-1/yr`,
     R_med = `R-E06`, R_min = `Rlow-E06`, R_max = `Rhigh-E06`
   )
+
+# Years for the time series
+yr_range <- range(sim_dat$Year)
+
+# Recruitment age
+age_recruit <- 2
+
+# Confidence interval
+ci_values <- c(0.05, 0.95)
+ci_pct <- (ci_values[2] - ci_values[1]) * 100
